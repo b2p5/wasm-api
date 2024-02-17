0231abf7513d836ff3c4f2d8ad6d820c47bbb577
b2p5 <bc-p5js@protonmail.com>
2024-02-17 16:41:38 +0100

    V0.5

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,23 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use wasm_bindgen_futures::{JsFuture, spawn_local};
 use web_sys::{Request, RequestInit, RequestMode, Response, window};
+use serde::Deserialize;
+use serde_wasm_bindgen::from_value;
+
+#[derive(Deserialize)]
+struct Fees {
+    #[serde(rename = "fastestFee")]
+    fastest_fee: u32,
+    #[serde(rename = "halfHourFee")]
+    half_hour_fee: u32,
+    #[serde(rename = "hourFee")]
+    hour_fee: u32,
+    #[serde(rename = "economyFee")]
+    economy_fee: u32,
+    #[serde(rename = "minimumFee")]
+    minimum_fee: u32,
+}
+
 
 // Esta función maneja la solicitud a la API y la actualización del DOM
 async fn fetch_price_and_update_dom() -> Result<(), JsValue> {
@@ -10,53 +27,44 @@ async fn fetch_price_and_update_dom() -> Result<(), JsValue> {
     opts.mode(RequestMode::Cors);
 
     let request = Request::new_with_str_and_init(
-        //"http://numbersapi.com/random",
         "https://mempool.space/api/v1/fees/recommended",
         &opts,
     )?;
 
     let window = window().unwrap();
-    let response = JsFuture::from(window.fetch_with_request(&request)).await?;
-    let response: Response = response.dyn_into().unwrap();
-    let text = JsFuture::from(response.text()?).await?.as_string().unwrap();  
-    let fastest_fee = text.split("fastestFee").collect::<Vec<&str>>()[1];
-    println!("fastestFee: {}", fastest_fee);
-    let half_hour_fee = text.split("halfHourFee").collect::<Vec<&str>>()[1];
-    println!("halfHourFee: {}", half_hour_fee);
-    let hour_fee = text.split("hourFee").collect::<Vec<&str>>()[1];
-    let economy_fee = text.split("economyFee").collect::<Vec<&str>>()[1];
-    let minimum_fee = text.split("minimumFee").collect::<Vec<&str>>()[1];
-    println!("minimumFee: {}", minimum_fee);
-
-
-    let document = window.document().unwrap();
-    let body = document.body().unwrap();
-
-    // Verifica si el elemento <p> ya existe
-    let existing_p = document.get_element_by_id("fees_info");
-
-    // Si existe, actualiza su contenido. Si no, crea uno nuevo.
-    let p = if let Some(p) = existing_p {
-        p
-    } else {
-        let p = document.create_element("p")?;
-        p.set_id("fees_info"); // Asigna un ID único
-        body.prepend_with_node_1(&p)?;
-        p
+    let response = match JsFuture::from(window.fetch_with_request(&request)).await {
+        Ok(res) => res,
+        Err(e) => {
+            log(&format!("Error fetching data: {:?}", e));
+            return Err(e);
+        }
     };
 
-    // Actualiza el contenido del elemento <p>
-    p.set_inner_html(&format!(
-        "fastest Fee: {}\n halfHour Fee: {}\n hour Fee: {}\n economy Fee: {}\n minimum Fee: {}\n\n",
-        fastest_fee, half_hour_fee, hour_fee, economy_fee, minimum_fee
-    ));
-
-    // let document = window.document().unwrap();
-    // let body = document.body().unwrap();
-    // let p = document.create_element("p")?;
-    // p.set_inner_html(&format!("fastest Fee:{}\n halfHour Fee:  {}\n hour Fee: {}\n economy Fee: {}\n minimum Fee: {}\n\n", fastest_fee, half_hour_fee, hour_fee, economy_fee, minimum_fee));
+    // Hora actual en formato hora:minutos:segundos - dia/mes/año
+    let date = js_sys::Date::new_0();
+    let date = date.to_locale_string("es-ES", &JsValue::UNDEFINED).as_string().unwrap();
     
-    // body.prepend_with_node_1(&p)?;
+    let response: Response = response.dyn_into().unwrap();
+    let fasstest_fee = JsFuture::from(response.json()?).await?;
+    let fees: Fees = from_value(fasstest_fee).unwrap();
+
+    let fastest_fee = fees.fastest_fee.to_string();  
+    let half_hour_fee = fees.half_hour_fee.to_string(); 
+    let hour_fee = fees.hour_fee.to_string();  
+    let economy_fee = fees.economy_fee.to_string();  
+    let minimum_fee = fees.minimum_fee.to_string();  
+    
+    let document = window.document().unwrap();
+    let body = document.body().unwrap();
+    let p = document.create_element("p")?;
+    p.set_inner_html(&format!("<b>Fecha:{}</b><br> fastest Fee:{}<br> halfHour Fee:  {}<br> hour Fee: {}<br> economy Fee: {}<br> minimum Fee: {}<hr><br>", 
+                                date, fastest_fee, half_hour_fee, hour_fee, economy_fee, minimum_fee));
+
+
+    body.prepend_with_node_1(&p)?;
+
+    //log(&format!("fastestFee: {:?}", fasstest_fee));
+
     
     Ok(())
 
@@ -76,7 +84,7 @@ pub fn run() -> Result<(), JsValue> {
 
     window().unwrap().set_interval_with_callback_and_timeout_and_arguments_0(
         closure.as_ref().unchecked_ref(),
-        30000, // Intervalo de tiempo en milisegundos
+        60000, // Intervalo de tiempo en milisegundos
     )?;
     closure.forget(); // Evita que la closure sea recolectada por el garbage collector
 
